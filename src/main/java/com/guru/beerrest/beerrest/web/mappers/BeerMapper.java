646a557d45ebec3382f3315f133743b4bd1c9141
B00ttichelli <donttrytofish@protonmail.com>
2021-09-23 03:00:08 +0300

    PostMapping for BeerDto

---
+++ src/main/java/com/guru/beerrest/beerrest/web/mappers/BeerMapper.java
@@ -0,0 +1,11 @@
+package com.guru.beerrest.beerrest.web.mappers;
+
+import com.guru.beerrest.beerrest.domain.Beer;
+import com.guru.beerrest.beerrest.web.model.BeerDto;
+import org.mapstruct.Mapper;
+
+@Mapper
+public interface BeerMapper {
+    BeerDto beerToBeerDTO(Beer beer);
+    Beer BeerDtoToBeer(BeerDto beerDto);
+}
